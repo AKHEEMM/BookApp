0450db98e27b87ffa3dd17105f17430654b969c1
skull-portall <skullportall@gmail.com>
2025-11-23 07:09:54 -0800

    another commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.app')
+@section('title', 'about - Page')
+
+@section('content')
+    <h1>About us</h1>
+    <p>Your gateway to a world of books.</p>
+@endsection
+
---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.app')
+@section('title', 'Welcome Page')
+
+@section('content')
+    <div class="container">
+        
+    </div>
+@endsection
